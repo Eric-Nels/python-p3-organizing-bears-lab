9ad34500bb4d151a6e77232f804119ee7485545a
Eric Nelson <ernelson12o0@gmail.com>
2023-10-24 14:18:56 -0400

    Lab Complete

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'Brown', 'Hungry', 'Grumpy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'Black', 'Hyper', 'Happy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'Black', 'Happy', 'Energetic', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'Brown', 'Lazy', 'Lazy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Black and White', 'Silly', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'Green', 'Grumpy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'Black', 'Sweet', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, 'M', 'White', 'Mean', True);
